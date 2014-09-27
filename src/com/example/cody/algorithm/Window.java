d2f7139df75c2d615168b2235837d5c491248c28
Vikont133 <vikont@frtk.ru>
2014-09-28 01:43:54 +0400

    Add hann window

--- src/com/example/cody/algorithm/Window.java
+++ src/com/example/cody/algorithm/Window.java
@@ -10,6 +10,14 @@ public class Window {
 		return x;
 	}
 	
+	public static double[] hann(double[] x){
+		int l = x.length;
+		for (int i = 0; i < l; i++){
+			x[i] *= 0.5 - 0.5 * Math.cos((2 * Math.PI * i)/(l - 1));
+		}
+		return x;	
+	}
+	
 	public static void main(String[] args) {
 		double[] x = {1, 2, 3, 4, 5};
 		x = Window.hamming(x);
